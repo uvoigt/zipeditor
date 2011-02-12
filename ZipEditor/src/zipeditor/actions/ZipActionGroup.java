2911699487331d70fa72503f30afd3d33b8dc42b
uwe_ewald <>
2011-02-12 08:31:18 +0000

    added copy action

--- ZipEditor/src/zipeditor/actions/ZipActionGroup.java
+++ ZipEditor/src/zipeditor/actions/ZipActionGroup.java
@@ -10,7 +10,9 @@ import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IWorkbenchCommandConstants;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.actions.ActionGroup;
 
@@ -19,6 +21,8 @@ import zipeditor.ZipEditor;
 
 public class ZipActionGroup extends ActionGroup {
 	private IAction fAddAction;
+	private IAction fCopyAction;
+	private IAction fCopyQualifiedAction;
 	private IAction fDeleteAction;
 	private IAction fExtractAction;
 	private IAction fSortAction;
@@ -27,17 +31,25 @@ public class ZipActionGroup extends ActionGroup {
 	private IAction fPreferencesAction;
 	private IAction fPropertiesAction;
 	private ZipEditor fEditor;
+	private Clipboard fClipboard;
 	
 	public ZipActionGroup(ZipEditor editor) {
 		fRevertAction = new RevertAction(editor);
+		fRevertAction.setActionDefinitionId(IWorkbenchCommandConstants.FILE_REVERT);
 		fSaveAction = new SaveAction(editor);
+		fSaveAction.setActionDefinitionId(IWorkbenchCommandConstants.FILE_SAVE);
 		fPreferencesAction = new PreferencesAction(editor);
 
 		fEditor = editor;
+		fClipboard = new Clipboard(fEditor.getViewer().getControl().getDisplay());
 	}
 	
 	public void fillContextMenu(IMenuManager menu) {
 		lazilyCreateActions();
+		menu.add(new Separator());
+		menu.add(fCopyAction);
+		menu.add(fCopyQualifiedAction);
+		menu.add(fDeleteAction);
 		menu.add(new Separator());
 		menu.add(fSaveAction);
 		menu.add(fRevertAction);
@@ -45,22 +57,30 @@ public class ZipActionGroup extends ActionGroup {
 		menu.add(fAddAction);
 		menu.add(fExtractAction);
 		menu.add(new Separator());
-		menu.add(fDeleteAction);
-		menu.add(new Separator());
 		menu.add(fPropertiesAction);
 		
 		updateActionBars();
 	}
 	
     private void lazilyCreateActions() {
-		if (fPropertiesAction == null)
+		if (fPropertiesAction == null) {
 			fPropertiesAction = new MultiPropertyDialogAction(fEditor.getSite(), fEditor.getViewer());
+			fPropertiesAction.setActionDefinitionId(IWorkbenchCommandConstants.FILE_PROPERTIES);
+		}
 		if (fAddAction == null)
 			fAddAction = new AddAction(fEditor.getViewer());
 		if (fExtractAction == null)
 			fExtractAction = new ExtractAction(fEditor.getViewer());
-		if (fDeleteAction == null)
+		if (fCopyAction == null) {
+			fCopyAction = new CopyAction(fEditor.getViewer(), false, fClipboard);
+			fCopyAction.setActionDefinitionId(IWorkbenchCommandConstants.EDIT_COPY);
+		}
+		if (fCopyQualifiedAction == null)
+			fCopyQualifiedAction = new CopyAction(fEditor.getViewer(), true, fClipboard);
+		if (fDeleteAction == null) {
 			fDeleteAction = new DeleteAction(fEditor.getViewer());
+			fDeleteAction.setActionDefinitionId(IWorkbenchCommandConstants.EDIT_DELETE);
+		}
 		if (fSortAction == null)
 			fSortAction = new SortAction(fEditor.getViewer(), PreferenceConstants.PREFIX_EDITOR);
 	}
@@ -83,6 +103,7 @@ public class ZipActionGroup extends ActionGroup {
     public void fillActionBars(IActionBars actionBars) {
 		lazilyCreateActions();
 
+		actionBars.setGlobalActionHandler(ActionFactory.COPY.getId(), fCopyAction);
 		actionBars.setGlobalActionHandler(ActionFactory.DELETE.getId(), fDeleteAction);
 		actionBars.setGlobalActionHandler(ActionFactory.SAVE.getId(), fSaveAction);
 		actionBars.setGlobalActionHandler(ActionFactory.REVERT.getId(), fRevertAction);
@@ -97,9 +118,16 @@ public class ZipActionGroup extends ActionGroup {
 		boolean empty = selection.isEmpty();
 		
 		lazilyCreateActions();
+		fCopyAction.setEnabled(!empty);
+		fCopyQualifiedAction.setEnabled(!empty);
 		fDeleteAction.setEnabled(!empty);
 		fSaveAction.setEnabled(fEditor.isDirty());
 		fRevertAction.setEnabled(fEditor.isDirty());
 		fPropertiesAction.setEnabled(!empty);
+	}
+	
+	public void dispose() {
+		super.dispose();
+		fClipboard.dispose();
 	}
 }
