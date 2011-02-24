c56c3cb3674863db66ccbadae0c357d9c8667513
uwe_ewald <>
2011-02-24 20:15:45 +0000

    fix folder creation when dragging folders

--- ZipEditor/src/zipeditor/ZipEditorDragAdapter.java
+++ ZipEditor/src/zipeditor/ZipEditorDragAdapter.java
@@ -33,7 +33,10 @@ public class ZipEditorDragAdapter extends DragSourceAdapter {
 			final File tmpDir = nodes[0].getModel().getTempDir();
 			for (int i = 0; i < nodes.length; i++) {
 				Node node = nodes[i];
-				fTempPaths[i] = new File(tmpDir, node.getFullPath()).getAbsolutePath();
+				File file = new File(tmpDir, node.getFullPath());
+				fTempPaths[i] = file.getAbsolutePath();
+				if (node.isFolder())
+					file.mkdirs();
 			}
 			extractor = new Thread(new Runnable() {
 				public void run() {
