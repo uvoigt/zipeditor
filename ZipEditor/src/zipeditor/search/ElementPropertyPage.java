21e1754552bad0b2cc46d9b4264950fc6a7c0695
uwe_ewald <>
2017-05-24 08:05:07 +0000

    wrap path field

--- ZipEditor/src/zipeditor/search/ElementPropertyPage.java
+++ ZipEditor/src/zipeditor/search/ElementPropertyPage.java
@@ -7,18 +7,19 @@ package zipeditor.search;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.content.IContentType;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.PropertyPage;
 
 public class ElementPropertyPage extends PropertyPage {
 
-	private Text fEntry;
-	private Text fPath;
-	private Text fType;
+	private StyledText fEntry;
+	private StyledText fPath;
+	private StyledText fType;
 
 	protected Control createContents(Composite parent) {
 		Composite control = new Composite(parent, SWT.NONE);
@@ -28,20 +29,31 @@ public class ElementPropertyPage extends PropertyPage {
 
 		Label label = new Label(control, SWT.LEFT);
 		label.setText(SearchMessages.getString("ElementPropertyPage.0")); //$NON-NLS-1$
-		fEntry = new Text(control, SWT.LEFT | SWT.READ_ONLY);
+		label.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false));
+		fEntry = new StyledText(control, SWT.LEFT | SWT.READ_ONLY);
 		fEntry.setText(element.getFileName());
+		fEntry.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+		fEntry.setBackground(control.getBackground());
 
 		label = new Label(control, SWT.LEFT);
 		label.setText(SearchMessages.getString("ElementPropertyPage.1")); //$NON-NLS-1$
-		fPath = new Text(control, SWT.LEFT | SWT.READ_ONLY);
+		label.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false));
+		fPath = new StyledText(control, SWT.LEFT | SWT.READ_ONLY | SWT.WRAP);
 		fPath.setText(element.getPath());
+		GridData data = new GridData(SWT.FILL, SWT.TOP, true, false);
+		data.widthHint = convertWidthInCharsToPixels(60);
+		fPath.setLayoutData(data);
+		fPath.setBackground(control.getBackground());
 
 		label = new Label(control, SWT.LEFT);
 		label.setText(SearchMessages.getString("ElementPropertyPage.2")); //$NON-NLS-1$
-		fType = new Text(control, SWT.LEFT | SWT.READ_ONLY);
+		label.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false));
+		fType = new StyledText(control, SWT.LEFT | SWT.READ_ONLY);
 		IContentType contentType = Platform.getContentTypeManager().findContentTypeFor(element.getFileName());
 		fType.setText(contentType.getName());
-	
+		fType.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+		fType.setBackground(control.getBackground());
+
 		applyDialogFont(control);
 		return control;
 	}
