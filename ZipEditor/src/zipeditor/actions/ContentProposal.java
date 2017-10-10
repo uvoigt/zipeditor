c461732039ce08ded90736b1621cfae1d7f55bc4
uwe_ewald <>
2017-10-10 08:12:25 +0000

    added file property

--- ZipEditor/src/zipeditor/actions/ContentProposal.java
+++ ZipEditor/src/zipeditor/actions/ContentProposal.java
@@ -8,11 +8,16 @@ class ContentProposal implements IContentProposal {
 	private String label;
 	private String description;
 	private int cursorPosition;
+	private Object file;
 	public ContentProposal(String content, String label, String description) {
 		this.content = content;
 		this.label = label;
 		this.description = description;
 		cursorPosition = content.length();
+	}
+	public ContentProposal(String content, String label, String description, Object file) {
+		this(content, label, description);
+		this.file = file;
 	}
 	public String getContent() {
 		return content;
@@ -25,5 +30,8 @@ class ContentProposal implements IContentProposal {
 	}
 	public String getDescription() {
 		return description;
+	}
+	public Object getFile() {
+		return file;
 	}
 }
