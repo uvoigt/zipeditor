6dfc2bd1783751bebb1b6698b93217e95b101e30
uwe_ewald <>
2017-10-11 11:33:30 +0000

    select match in zip editor

--- ZipEditor/src/zipeditor/search/OpenArchiveAction.java
+++ ZipEditor/src/zipeditor/search/OpenArchiveAction.java
@@ -8,21 +8,26 @@ import java.io.File;
 
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 
-import zipeditor.LocalFileEditorInput;
 import zipeditor.Utils;
+import zipeditor.ZipEditor;
 import zipeditor.ZipEditorPlugin;
+import zipeditor.model.Node;
 
 public class OpenArchiveAction extends Action {
 
 	private IWorkbenchPage fPage;
 	private File fFile;
+	private Node fNodeSelection;
 
-	public OpenArchiveAction(IWorkbenchPage page, File file) {
+	public OpenArchiveAction(IWorkbenchPage page, File file, Node nodeSelection) {
 		fPage = page;
 		fFile = file;
+		fNodeSelection = nodeSelection;
 		setText(SearchMessages.getString("OpenArchiveAction.0")); //$NON-NLS-1$
 		setImageDescriptor(ZipEditorPlugin.getImageDescriptor("icons/zipicon.gif")); //$NON-NLS-1$
 	}
@@ -30,7 +35,10 @@ public class OpenArchiveAction extends Action {
 	public void run() {
 		IFileStore fileStore = Utils.getFileStore(fFile);
 		try {
-			fPage.openEditor(new LocalFileEditorInput(fileStore), "zipeditor.ZipEditor"); //$NON-NLS-1$
+			IEditorPart editor = fPage.openEditor(Utils.createEditorInput(fileStore), "zipeditor.ZipEditor"); //$NON-NLS-1$
+			if (editor instanceof ZipEditor && fNodeSelection != null) {
+				((ZipEditor) editor).getViewer().setSelection(new StructuredSelection(fNodeSelection), true);
+			}
 		} catch (PartInitException e) {
 			ZipEditorPlugin.log(e);
 		}
